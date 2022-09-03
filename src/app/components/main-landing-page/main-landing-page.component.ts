ebc946708cab2b06ee9c29933f01a8f8c5bd8890
tluwaniMS <tluwani2020@gmail.com>
2022-09-03 15:41:07 +0200

    adding-main-landing-page

---
+++ src/app/components/main-landing-page/main-landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-landing-page',
+  templateUrl: './main-landing-page.component.html',
+  styleUrls: ['./main-landing-page.component.scss']
+})
+export class MainLandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
